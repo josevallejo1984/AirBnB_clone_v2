59e8dc3267cc4ed995a886d9dde5a8514e7d4b1b
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-17 16:41:41 -0500

    sd

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,31 @@
 #!/usr/bin/python3
-"""
-A Fabric script (based on the file 1-pack_web_static.py) that distributes
-an archive to my web servers, using the function do_deploy.
-"""
-import os.path
-from fabric.api import *
-from fabric.operations import run, put, sudo
+"""Distributes an archive to your web servers, using the function do_deploy"""
+from fabric.contrib import files
+from fabric.api import env, put, run
+import os
 
-"""my server's ip addresses"""
 env.hosts = ['35.231.89.82', '54.226.196.8']
 
 
 def do_deploy(archive_path):
-    """do_deploy function."""
-    if (os.path.isfile(archive_path) is False):
+    """Function for deploy"""
+    if not os.path.exists(archive_path):
         return False
 
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
+
     try:
-        file_name = archive_path.split("/")[-1]
-        new_folder = ("/data/web_static/releases/" + file_name.split(".")[0])
-
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(new_folder))
-        run("sudo tar -xzf /tmp/{} -C {}".
-            format(file_name, new_folder))
-        run("sudo rm /tmp/{}".format(file_name))
-        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
-        run("sudo rm -rf {}/web_static".format(new_folder))
-        run('sudo rm -rf /data/web_static/current')
-        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        put(archive_path, '/tmp')
+        run('mkdir -p {}'.format(dest))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('rm -f /tmp/{}.tgz'.format(name))
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+        run('rm -rf {}/web_static'.format(dest))
+        run('rm -rf /data/web_static/current'.format(name))
+        run('ln -s {} /data/web_static/current'.format(dest))
         return True
-
     except:
         return False
