f435e4b49107bfad5cc456d7ac37a582bb951f41
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-17 14:54:39 -0500

    fix code in install nginx.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,21 +2,22 @@
 # Script that sets up your web servers for the deployment of web_static. It must:
 if ! which nginx > /dev/null
 then
-	sudo apt-get -y update
+	sudo apt-get update
 	sudo apt-get -y install nginx
 fi
 # create folder recursive if not exist
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
 # create a fake file html
-echo -e '<html>
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>' > /data/web_static/releases/test/index.html
-# create simbolic link
-sudo ln -s /data/web_static/releases/test /data/web_static/current
+</html>" > /data/web_static/releases/test/index.html
+# create simbolic link and re-create if exit (option f forced)
+ln -sf /data/web_static/releases/test /data/web_static/current
 # change ownership to folder /data and all you content
 sudo chown -Rh ubuntu:ubuntu /data/
 # config default file nginx for server to aliases
