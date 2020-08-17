aee5bd3e42c641c80c96c1ea5d76242edea0e87b
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-17 15:15:18 -0500

    create task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Desploy site web."""
+import os.path
+from fabric.api import *
+from fabric.operations import run, put
+env.hosts = ['66.70.184.210', '142.44.164.128']
+
+
+def do_deploy(archive_path):
+    """Upload file in the server remote."""
+    if (os.path.isfile(archive_path) is False):
+        return False
+
+    try:
+        put(archive_path, "/tmp/")
+        unpack = archive_path.split("/")[-1]
+        folder = ("/data/web_static/releases/" + unpack.split(".")[0])
+        run("sudo mkdir -p {:s}".format(folder))
+
+        run("sudo tar -xzf /tmp/{:s} -C {:s}".format(unpack, folder))
+
+        run("sudo rm /tmp/{:s}".format(unpack))
+        run("sudo mv {:s}/web_static/* {:s}/".format(folder, folder))
+        run("sudo rm -rf {:s}/web_static".format(folder))
+
+        run('sudo rm -rf /data/web_static/current')
+
+        run("sudo ln -s {:s} /data/web_static/current".format(folder))
+        return True
+    except:
+        return False
