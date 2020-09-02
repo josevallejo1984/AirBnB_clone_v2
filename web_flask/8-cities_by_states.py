2af3cd0dc9dc27c6fc70dbbefbacda51715fa796
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-02 12:00:13 -0500

    create task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,6 +4,7 @@ starts a Flask web app.
 """
 from models import storage
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
@@ -14,14 +15,15 @@ def teardown(exception):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route("/cities_by_states", strict_slashes=False)
 def route1():
     """Display HTML page with a list of
     all objects inside a DBstorage."""
-    url = '7-states_list.html'
+    url = '8-cities_by_states.html'
     dict_states = storage.all("State")
-    return render_template(url, states=dictionary)
+    dict_cities = storage.all("City")
+    return render_template(url, states=dict_states,  cities=dict_cities)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port="5000")
+    app.run(host='0.0.0.0', port="5000", debug=True)
