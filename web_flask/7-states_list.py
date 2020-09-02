5c25f2c2514196e9759f858a229ccdf6e80cc63a
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-01 23:08:36 -0500

    more fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,44 +1,27 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+"""
+starts a Flask web app.
+"""
 from models import storage
-from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_state():
-    '''
-    Lists cities by the state
-    it is in
-    '''
-    state_dict = storage.all('State')
-    state_list = []
-    for state in state_dict.values():
-        state_list.append(state)
-    return render_template('8-cities_by_states.html', state_list=state_list)
-
-
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    '''
-    Lists states from db
-    '''
-    state_dict = storage.all('State')
-    state_list = []
-
-    for state in state_dict.values():
-        state_list.append(state)
-    return render_template('7-states_list.html', state_list=state_list)
-
-
 @app.teardown_appcontext
-def teardown_app(e):
-    '''
-    teardown app context
-    '''
+def teardown(exception):
+    """Removes current SQLAlchemy session."""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/states_list", strict_slashes=False)
+def route1():
+    """Display HTML page with a list of
+    all objects inside a DBstorage."""
+    url = '7-states_list.html'
+    dictionary = storage.all("State")
+    return render_template(url, states=dictionary)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port="5000")
