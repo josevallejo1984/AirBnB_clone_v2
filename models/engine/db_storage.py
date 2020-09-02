199eb264d57cdb05166649b08450f3265aa0b9a7
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-01 23:02:12 -0500

    update and fix problem in dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -10,14 +10,12 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-
-
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 class DBStorage():
     """DBStorage."""
-
     __engine = None
     __session = None
-
     def __init__(self):
         """Init method."""
         mysql_user = getenv('HBNB_MYSQL_USER')
@@ -25,65 +23,42 @@ class DBStorage():
         mysql_host = getenv('HBNB_MYSQL_HOST')
         mysql_db = getenv('HBNB_MYSQL_DB')
         mysql_env = getenv('HBNB_ENV')
-
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
             mysql_user, mysql_pwd, mysql_host, mysql_db), pool_pre_ping=True)
-
         """If var HBNB_ENV == test, then DROP all the tables."""
         if mysql_env == 'test':
             Base.metadata.drop_all(self.__engine)
-
     def all(self, cls=None):
-        """All method - retrieves an object representation.
-
-        Args:
-            cls [object]: Instance of a class.
-
-        Return:
-            new_dict [dict]: Dictionary with all object representations.
-        """
-        if cls is not None:
-            objs = self.__session.query(cls).all()
-
-        else:
-            classes = ['State', 'City', 'User', 'Place', 'Review', 'amenity']
-            objs = []
-            for _class in classes:
-                objs += self.__session.query(eval(_class)).all()
-
-        """creating a new dictionary and saving data"""
+        """query on the current database session"""
         new_dict = {}
-
-        for obj in objs:
-            key = '{}.{}'.format(type(obj).__name__, obj.id)
-            new_dict[key] = obj
-
-        return new_dict
-
+        objs = [v for k, v in classes.items()]
+        if cls:
+            if isinstance(cls, str):
+                cls = classes[cls]
+            objs = [cls]
+        for c in objs:
+            for instance in self.__session.query(c):
+                key = str(instance.__class__.__name__) + "." + str(instance.id)
+                new_dict[key] = instance
+        return (new_dict)
     def new(self, obj):
         """Add the object to the current. database session (self.__session)."""
         if obj:
             self.__session.add(obj)
-
     def save(self):
         """Commit all changes of the current. database session."""
         self.__session.commit()
-
     def delete(self, obj=None):
         """Delete from the current database session obj if not None."""
         if obj:
             self.__session.delete(obj)
-
     def reload(self):
         """Create all tables in the database (feature of SQLAlchemy)."""
         Base.metadata.create_all(self.__engine)
-
         self.__session = sessionmaker(bind=self.__engine,
                                       expire_on_commit=False)
-
         Session = scoped_session(self.__session)
         self.__session = Session()
-
     def close(self):
-        """Metodo close"""
+        """Close SQLAlchemy actual session"""
         self.__session.close()
